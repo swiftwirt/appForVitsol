0dd0352b1dbd2c83692a913732394ca975fced1d
Dmitry Ivashin <dmitroivashyn@gmail.com>
2017-04-03 15:04:17 +0300

    Initial Commit

---
+++ AppForVitsolutions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppForVitsolutions
+//
+//  Created by Ivashin Dmitry on 4/3/17.
+//  Copyright © 2017 Ivashin Dmitry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
