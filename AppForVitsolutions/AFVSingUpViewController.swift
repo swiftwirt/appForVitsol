724798b102d053a40c35f7d7189d086e21296924
Dmitry Ivashin <dmitroivashyn@gmail.com>
2017-04-03 19:37:12 +0300

    back up

---
+++ AppForVitsolutions/AFVSingUpViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AFVSingUpViewController.swift
+//  AppForVitsolutions
+//
+//  Created by Ivashin Dmitry on 4/3/17.
+//  Copyright © 2017 Ivashin Dmitry. All rights reserved.
+//
+
+import UIKit
+
+class AFVSingUpViewController: UITableViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+}
