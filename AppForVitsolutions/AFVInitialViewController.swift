35f3286ba9eddb5bbcf28ec7db8c3a642917406f
Dmitry Ivashin <dmitroivashyn@gmail.com>
2017-04-03 16:27:14 +0300

    AFV-1 > Add basic presentation skeleton

---
+++ AppForVitsolutions/AFVInitialViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AFVInitialViewController.swift
+//  AppForVitsolutions
+//
+//  Created by Ivashin Dmitry on 4/3/17.
+//  Copyright © 2017 Ivashin Dmitry. All rights reserved.
+//
+
+import UIKit
+
+class AFVInitialViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
